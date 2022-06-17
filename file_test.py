650fbbd174cc1368805716123930d0ce82eb3ec0
DCohen07 <83410679+DCohen07@users.noreply.github.com>
2022-06-17 13:55:33 +0200

    test gitaction flake8

--- file_test.py
+++ file_test.py
@@ -1,20 +1,20 @@
 import pytest
  
 def test_calc_addition():
-    "Verify the output of 'calc_addition' function"
+    
     output = 2+4
     assert output == 6
  
 def test_calc_substraction():
-    "Verify the output of `calc_substraction` function"
+    
     output = 2-4
     assert output == -2
      
 def test_calc_multiply():
-    "Verify the output of `calc_multiply` function"
+    
     output = 2*4
     assert output == 8
 
 def test_coucou():
-    output='cou'
-    assert output == 'cou'
+    output='hello'
+    assert output == 'hello'
